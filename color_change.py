cdb2dcc9911877f6ea05a18fd97e4fd54288ea73
austinjs <ajsprunger@gmail.com>
2022-05-04 16:08:30 -0600

    tweak ranges for color testing

--- color_change.py
+++ color_change.py
@@ -1,17 +1,17 @@
 from PIL import Image, ImageColor
 
-img = Image.open('images/squirtle.jpg')
+img = Image.open('images/pikachu.jpg')
 img = img.convert('RGB')
 
 d = img.getdata()
 
 new_image = []
 for item in d:
-  if item[0] in list(range(200, 256)):
-    new_image.append((255, 224, 100))
+  if item[0] in list(range(200, 250)):
+    new_image.append((142, 195, 208))
   else:
     new_image.append(item)
 
 img.putdata(new_image)
 
-img.save('squirtle_altered.jpg')
+img.save('pikachu_altered.jpg')
