15f3aa7c9bab5f7fa7f60907658a74c6a09c370c
austinjs <ajsprunger@gmail.com>
2022-05-04 15:32:35 -0600

    add rotation

---
+++ rotate.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import os, sys
+
+for file_in in sys.argv[1:]:
+  f, e = os.path.splitext(file_in)
+  file_out = f + '_rotated.jpg'
+  if file_in != file_out:
+    try:
+      with Image.open(file_in) as image:
+        image.convert("RGB").rotate(90).save(file_out)
+        print(f"successfully converted {file_in}")
+    except OSError as error:
+      print(error)
+      print('cannot convert', file_in)
