8136848ea108aeb457ceb20f16e448adfa69a8af
austinjs <ajsprunger@gmail.com>
2022-05-05 15:55:43 -0600

    add basic html

--- color_change.py
+++ color_change.py
@@ -1,17 +1,17 @@
 from PIL import Image, ImageColor
 
-img = Image.open('images/pikachu.jpg')
+img = Image.open('images/Headshot_altered.jpg')
 img = img.convert('RGB')
 
 d = img.getdata()
 
 new_image = []
 for item in d:
-  if item[0] in list(range(240, 256)) and item[1] in list(range(40, 60)) and item[2] in list(range(20, 45)):
+  if item[0] in list(range(30, 100)) and item[1] in list(range(60, 150)) and item[2] in list(range(100, 150)):
     new_image.append((142, 195, 208))
   else:
     new_image.append(item)
 
 img.putdata(new_image)
 
-img.save('pikachu_altered.jpg')
+img.save('Headshot_altered.jpg')
