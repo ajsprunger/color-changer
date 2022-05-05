82194c72be3f298cba593438ab2131322c13f3c1
austinjs <ajsprunger@gmail.com>
2022-05-05 14:58:33 -0600

    add third range for rgb

--- color_change.py
+++ color_change.py
@@ -7,7 +7,7 @@ d = img.getdata()
 
 new_image = []
 for item in d:
-  if item[0] in list(range(200, 250)):
+  if item[0] in list(range(240, 256)) and item[1] in list(range(40, 60)) and item[2] in list(range(20, 45)):
     new_image.append((142, 195, 208))
   else:
     new_image.append(item)
